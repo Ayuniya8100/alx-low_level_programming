d10505882cc087ed02ffadee704ccc96562d6e7e
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-10-13 19:59:50 +0000

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main: Print puts programming
+ * main - entry level
+ * Description: Print file without printf function
  * Return: Return 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
