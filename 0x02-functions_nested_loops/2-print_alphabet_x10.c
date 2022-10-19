1dbea5424b145062000bc59c5220cb5c12294e71
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-10-19 10:20:30 +0000

    print lower case alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lower casealphabet ten time
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int x, lc;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (lc  = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+	}
+	_putchar('\n');
+}
