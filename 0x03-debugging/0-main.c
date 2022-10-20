904885395cb854132f42883fd35d931b98f2716c
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-10-20 15:01:21 +0000

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,40 +1,14 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
-  * positive_or_negative - prints if integer is positive or negative
-  * Return: 0
-  * @i: parameter
+  * main - prints if integer is positive or negative
+  * Return: always 0
   */
-
-void positive_or_negative(int i)
-{
-	int n;
-
-		srand(time(0));
-		n = i;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-		printf("%d is negative\n", n);
-}
-/**
- * main - debugging example
- * Return: always 0
- */
 int main(void)
 {
 	int i;
 
 	i = 0;
+
 	positive_or_negative(i);
 	return (0);
 }
