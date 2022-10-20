43fd4f1056542b4fc6d8f7a96a63332f544f8997
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-10-20 20:10:05 +0000

    0. isupper

--- 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,11 @@
 #include "main.h"
+
 /**
  * _isupper - print upper case character
  * @c: stands for character
  * Return: 0 or 1 depending on the condition
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c >= 'A' && c <= 'Z');
 }
