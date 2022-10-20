94631f9f406947649ccfd69b73a2380be59d04bc
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-10-20 22:16:26 +0000

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
 /**
   * print_numbers - print from 0 to 9
   */
-
 void print_numbers(void)
 {
 	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-		_putchar((x % 2) + '0');
+
+		_putchar((x % 10) + '0');
 	}
 	_putchar('\n');
 }
