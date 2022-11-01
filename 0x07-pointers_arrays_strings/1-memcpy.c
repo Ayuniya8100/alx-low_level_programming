885661b02c7cdbd17336244c53d8ee5d1f97ecc8
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-11-01 06:31:25 +0000

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - print copy of character
+ * @dest: adress where copy the file
+ * @src: adress where copied from
+ * @n: size of value to be copied
+ * Return: return dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[n] = src[n];
+	return (dest);
+}
