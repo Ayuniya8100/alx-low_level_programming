fc1293f1206e78f1e61d195b03869b00266dcfa0
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-11-03 19:43:13 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
  * _puts_recursion - print string by recursion
  * @s: strint to be printed
@@ -8,9 +6,6 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-	_puts_recursion(s);
-	puts(s);
-	}
+	if (*s == '\0')
+	_putchar('\n');
 }
