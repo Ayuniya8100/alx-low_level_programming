0e5dcab04abb8646174cedf5ca23f7ee22bcefac
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-11-16 07:22:37 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - its structure name
+ * @name: 1st member of the structure
+ * @age: 2nd member of the structure
+ * @owner:3rd  member of the structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
