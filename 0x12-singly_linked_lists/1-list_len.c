f4659686b61bde43179e4ff00c57d72d5e5013a1
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-11-29 09:13:08 +0000

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,2 +1,21 @@
 #include "lists.h"
+/**
+ * list_len - length of element
+ * @h: head
+ * Return: return number of element
+ */
 
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	int count = 0;
+
+	temp = h;
+
+	while (temp->next == NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
